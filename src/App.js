c6d048c753581398e85c32dfb8942d80405caaaa
iyedjdidi <58891244+iyedjdidi@users.noreply.github.com>
2023-02-14 12:43:56 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Home />
+      <Contact />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex-col justify-center h-full'>
+            <p>Hi, my name is</p>
+            <h1> Iyed Jdidi</h1>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
